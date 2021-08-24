d6e3558bb4157c62eab4a1d2ae08f92798a05e5c
Mahaa Santeep G <82812064+Santeep@users.noreply.github.com>
2021-08-25 04:42:50 +0530

    Create Capitalize First character.cpp

---
+++ C++ Programming/Capitalize First character.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+
+    int i;
+    char a[30];
+    cout<<"\nEnter any string:\n ";
+    gets(a);
+
+    int len = strlen(a);
+
+    for(i=0;i<len;++i)
+    {
+        if(i==0)
+        {
+            if(islower(a[i]))
+            a[i]=toupper(a[i]);
+        }
+        else
+        {
+            if(a[i]!=' ')
+            {
+                if(isupper(a[i]))
+                    a[i]=tolower(a[i]);
+            }
+            else
+            {
+                i++;
+                if(islower(a[i]))
+                    a[i]=toupper(a[i]);
+            }
+        }
+    }
+    cout<<"\nUpdated New String is : "<<a<<"\n";
+
+    return 0;
+}
